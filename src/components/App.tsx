037d3d59eeeb43c2ae34d54be28ca54441cad6da
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-06-25 18:32:09 +0200

    feat: move App to a components folder

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import { registerRootComponent } from 'expo';
+import { StatusBar } from 'expo-status-bar';
+import { Home } from './screens';
+
+function App() {
+  return (
+    <>
+      <Home />
+      <StatusBar />
+    </>
+  );
+}
+
+export default registerRootComponent(App);
