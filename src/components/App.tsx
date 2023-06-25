093e97e674e2b0bd8c3f8c90b2bd60aab5978df2
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-06-25 23:20:56 +0200

    deps: add tamagui

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,28 @@
+import { useColorScheme } from 'react-native';
 import { registerRootComponent } from 'expo';
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
+import { TamaguiProvider, Theme } from 'tamagui';
+import config from '../../tamagui.config';
 import Navigator from './Navigator';
 
 function App() {
+  const colorScheme = useColorScheme();
+
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  });
+
+  if (!loaded) return null;
+
   return (
-    <>
-      <Navigator />
-      <StatusBar />
-    </>
+    <TamaguiProvider config={config}>
+      <Theme name={colorScheme}>
+        <Navigator />
+        <StatusBar />
+      </Theme>
+    </TamaguiProvider>
   );
 }
 
