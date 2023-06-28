b6ef7ab933975105ba131e768beb1e17826dbc2e
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-06-28 22:58:37 +0200

    feat(components): add Settings screen

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -2,12 +2,17 @@ import type { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Home as HomeIcon, Group as GroupIcon } from '@tamagui/lucide-icons';
-import { Map, Clan } from './screens';
+import {
+  Home as HomeIcon,
+  Group as GroupIcon,
+  Settings as SettingsIcon,
+} from '@tamagui/lucide-icons';
+import { Map, Clan, Settings } from './screens';
 
 export type RootTabParamList = {
   Map: undefined;
   Clan: undefined;
+  Settings: undefined;
 };
 
 // eslint-disable-next-line prettier/prettier
@@ -24,6 +29,11 @@ export default function Navigator() {
           name="Clan"
           component={Clan}
           options={{ tabBarIcon: (props) => <GroupIcon /> }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={Settings}
+          options={{ tabBarIcon: (props) => <SettingsIcon /> }}
         />
       </Tab.Navigator>
     </NavigationContainer>
--- src/components/screens/index.ts
+++ src/components/screens/index.ts
@@ -1,2 +1,3 @@
 export { default as Map } from './Map';
 export { default as Clan } from './Clan';
+export { default as Settings } from './Settings';
